dc088baa75c09600e3e190e5a15bd666076da518
AdityaMishra3648 <123090809+AdityaMishra3648@users.noreply.github.com>
2024-02-08 18:09:01 +0530

    initial app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState("white")
+  let changeColor = (col) =>{
+    setColor(col);
+  }
   return (
-    <div>
-      hello this is a temporary file 
-      this fiel update
+    <div className="w-screen h-screen p-0 m-0" style={{backgroundColor : color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+
+        <button className='bg-green-500 outline-none rounded-full  px-4 py-1' onClick={()=>changeColor("green")}>Green</button>
+        <button className='bg-pink-500 outline-none rounded-full  px-4 py-1' onClick={()=>changeColor("pink")}>Pink</button>
+        <button className='bg-purple-500 outline-none rounded-full  px-4 py-1' onClick={()=>changeColor("purple")}>Purple</button>
+        <button className='bg-gray-500 outline-none rounded-full  px-4 py-1' onClick={()=>changeColor("gray")}>Gray</button>
+        <button className='bg-red-500 outline-none rounded-full  px-4 py-1' onClick={()=>changeColor("red")}>Red</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
+
